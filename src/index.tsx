dbdbd89f82cff09075537c37ee18760262709eb4
sark <sark@supersark.com>
2022-08-03 15:36:14 +0900

    7.2

--- src/index.tsx
+++ src/index.tsx
@@ -62,9 +62,11 @@ table {
 body {
   font-weight: 300;
   font-family: 'Source Sans Pro', sans-serif;
+  background: linear-gradient(180deg, #f08, #d0e);
+  /* background-color: white; */
   color:black;
   line-height: 1.2;
-  background:linear-gradient(135deg,#e09,#d0e);
+
 }
 a {
   text-decoration:none;
